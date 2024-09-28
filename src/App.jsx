060dfa4f6a6e7b2812ee146efc32d43a9fe4b591
Iqra327 <iqrarasheed219@gmail.com>
2024-09-28 20:32:03 +0500

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import './App.css';
+import React from 'react'
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Country from './pages/Country';
+import About from './pages/About';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: 'about',
+    element: <About />
+  },
+  {
+    path: 'country',
+    element: <Country />
+  },
+  {
+    path: 'contact',
+    element: <Contact />
+  }
+])
+
+const App = () => {
+  return <RouterProvider router={router}></RouterProvider>
+}
+
+export default App
